b578f3096e8ff0272dba3751b165af76398ae160
jotave8 <128985376+jotave8@users.noreply.github.com>
2025-03-14 14:29:38 +0000

    desafio12

---
+++ Mundo1/Desafio12.py
@@ -0,0 +1,5 @@
+#Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto
+
+valor = float(input('Informe o valor do produto: '))
+desconto = valor*0.95
+print('O produto custa R${:.2f} e aplicando o desconto de 5% fica por R${:.2f}'.format(valor, desconto))
