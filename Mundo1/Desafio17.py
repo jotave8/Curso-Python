6f241589eef310c3fabccf4b87616e1bee35fa16
jotave8 <128985376+jotave8@users.noreply.github.com>
2025-03-19 21:55:06 +0000

    aula8

--- Mundo1/Desafio17.py
+++ Mundo1/Desafio17.py
@@ -1,10 +1,9 @@
 '''Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo. Calcule e 
 mostre o comprimento da hipotenusa'''
-from math import sqrt
-
+from math import hypot
 
 print('Considerando um triângulo retângulo')
-cateopos = int(input('Informe o comprimento do cateto oposto: '))
-cateadja = int(input('Informe o comprimento do cateto adjacente: '))
-hipotenusa = sqrt((pow(cateopos,2)+pow(cateadja,2)))
-print('A hipotenusa desse triângulo retângulo é {:.2f}'.format(hipotenusa))
+co = int(input('Informe o comprimento do cateto oposto: '))
+ca = int(input('Informe o comprimento do cateto adjacente: '))
+hip = hypot(co,ca)
+print('A hipotenusa desse triângulo retângulo é {:.2f}'.format(hip))
--- Mundo1/Desafio21.py
+++ Mundo1/Desafio21.py
@@ -1,1 +1,8 @@
 #Faça um programa em python que abra e reproduza o áudio de um arquivo MP3
+
+import pygame
+
+pygame.init() #Inicia o pygame
+pygame.mixer.music.load() #Inserir o nome do arquivo entre aspas simples
+pygame.mixer.music.play() #Toca a música
+pygame.time.delay(30000) #Faz o programa continuar ativo pelo tempo determinado
