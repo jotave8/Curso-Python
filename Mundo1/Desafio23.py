4715504dc1d860914d13f7218b47503a881f83d9
jotave8 <128985376+jotave8@users.noreply.github.com>
2025-04-01 19:20:53 +0000

    criação dos arquivos dos desafios

---
+++ Mundo1/Desafio23.py
@@ -0,0 +1,2 @@
+''' Faça um programa que leia um número de 0 a 999 e mostre na tela cada um dos digitos separados em unidade, dezena
+centena e milhar'''
---
+++ Mundo1/Desafio26.py
@@ -0,0 +1,4 @@
+''' Faça um programa que leia um frase pelo teclado e mostre:
+- Quantas vezes aparece a letra 'R'
+- Em que posição ela aparece a primeira vez
+- Em que posição ela aparece a última vez '''
