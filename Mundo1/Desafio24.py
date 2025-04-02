e7e036bcc4eff0976062745b465aee471f854d56
jotave8 <128985376+jotave8@users.noreply.github.com>
2025-04-02 22:36:53 +0000

    Desafio24

--- Mundo1/Desafio24.py
+++ Mundo1/Desafio24.py
@@ -1,1 +1,7 @@
 ''' Crie um programa que leia o nome de uma cidade e diga se ela começa ou não como nome 'SANTO' '''
+
+city = str(input('Informe o nome da cidade: '))
+city = city.upper()
+space = city.find(" ")
+name = city[0:space]
+print('O nome da cidade começa com "SANTO": {}'.format("SANTO" in name))
